a32929e20d0311c2703e0fa4d4e2a8a71f61b80e
atul-ram <atul.ram@gmail.com>
2018-01-18 15:39:08 +0530

    Update base.tf

--- base.tf
+++ base.tf
@@ -1,5 +1,6 @@
 provider "vSphere" {
-	user = ""
-	password =""
-	vsphere_server = ""
+	user = "demouser@dcloud.cisco.com"
+	password ="C1sco12345"
+	vsphere_server = "vc1.dcloud.cisco.com"
 	allow_unverified_ssl= true
+	}
