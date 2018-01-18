511e08fdb915b292878c3edbef867cb5dbfdf879
atul-ram <atul.ram@gmail.com>
2018-01-18 17:27:06 +0530

    Update base.tf

--- base.tf
+++ base.tf
@@ -29,3 +29,27 @@ data "vsphere_virtual_machine" "template" {
   datacenter_id = "${data.vsphere_datacenter.dc.id}"
 }
 		  
+data "vsphere_network" "network" {
+  name          = ""
+  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+}
+
+
+resource "vsphere_virtual_machine" "vm" {
+  name             = "terraform-test"
+  resource_pool_id = "${data.vsphere_resource_pool.pool.id}"
+  datastore_id     = "${data.vsphere_datastore.datastore.id}"
+
+  num_cpus = 2
+  memory   = 4096
+  guest_id = "centos7guest"
+
+  network_interface {
+    network_id = "${data.vsphere_network.network.id}"
+  }
+
+  disk {
+    name = "terraform-test.vmdk"
+    size = 20
+  }
+}
