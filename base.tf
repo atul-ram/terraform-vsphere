2dbb0ef6070ec81c9f7682a6e916c67dc0ee711c
atul-ram <atul.ram@gmail.com>
2018-01-18 16:42:42 +0530

    Update base.tf

--- base.tf
+++ base.tf
@@ -8,6 +8,16 @@ provider "vSphere" {
 
 data "vsphere_datacenter" "dc" {}
 
+data "vsphere_datastore" "datastore" {
+  name          = "Datastore_NFS"
+  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+}
+
+data "vsphere_resource_pool" "pool" {
+  name          = "dCloud-DC/Resources"
+  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+}
+
 resource "vsphere_folder" "TerraformFrontEnd" {
       datacenter_id = "${data.vsphere_datacenter.dc.id}"
       path = "TerraformFrontEnd"
